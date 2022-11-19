1eec9efdabd5c93e87548341a9a231c65e5c3839
Bliss <blissarmah8@gmail.com>
2022-11-19 10:05:39 -0800

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, vero! Asperiores totam, omnis doloribus nemo in ea delectus nostrum. Inventore nesciunt eaque voluptas mollitia ipsam accusantium cum architecto deserunt dolorum perspiciatis sint quia reprehenderit a voluptatum libero illum, amet ut.
+      </p>
+    </section>
+  )
+}
+
+export default About
